8a0c42518f8c6ba0c1de8c469c5b8219f9f342f0
kkok01 <128560399+kkok01@users.noreply.github.com>
2024-12-27 10:17:15 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -13,7 +13,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 export LC_ALL=C
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b0'}
+export UUID=${UUID:-'1c36c9de-6d87-4f41-a11b-6f064d728918'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
